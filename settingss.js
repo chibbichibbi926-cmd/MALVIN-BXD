99515ca60b4d67745fbaf6e0a7aaf0882d73ae6b
chibbichibbi926-cmd <chibbichibbi926@gmail.com>
2025-09-03 22:29:37 +0530

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~knUiXTYD#wOYfh5YyjOmGczhWHBzALIQdsNhtFG0iycc7XGh8408",  Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "966597001591", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
